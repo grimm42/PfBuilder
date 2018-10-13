690ce71e7886889048b2d1d35b4ce9121c9d8deb
Nick Henkenjohann <nick.henkenjohann@gmail.com>
2018-10-13 16:17:45 +0200

    Added prism dependencies, created base shell for app

---
+++ PfBuilder/Bootstrapper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Practices.Unity;
+using PfBuilder.View;
+using Prism.Unity;
+using System.Windows;
+
+namespace PfBuilder
+{
+    class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<Shell>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+    }
+}
---
+++ PfBuilder/View/Shell.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace PfBuilder.View
+{
+    /// <summary>
+    /// Interaction logic for Shell.xaml
+    /// </summary>
+    public partial class Shell : Window
+    {
+        public Shell()
+        {
+            InitializeComponent();
+        }
+    }
+}
