3ef304298b844de0a7bdc77038763bdf756c571a
Nick Henkenjohann <nick.henkenjohann@gmail.com>
2018-10-13 22:30:31 +0200

    wip characterentity + some enums

---
+++ PfBuilder.Infrastructure/Enums/EnumUtil.cs
@@ -0,0 +1,11 @@
+﻿namespace PfBuilder.Infrastructure.Enums
+{
+    public static class EnumUtil
+    {
+        public enum GenderType { Male, Female };
+
+        public enum AbilityScore { Strength, Dexterity, Constitution , Intelligence, Wisdom, Charisma};
+
+        public enum AlignmentType { LawfulGood, NeutralGood, ChaoticGood, LawfulNeutral, Neutral, ChaoticNeutral, LawfulEvil, NeutralEvil, ChaoticEvil};
+    }
+}
---
+++ PfBuilder.Models/Entities/Character.cs
@@ -0,0 +1,50 @@
+﻿using PfBuilder.Infrastructure.Entity;
+using static PfBuilder.Infrastructure.Enums.EnumUtil;
+
+namespace PfBuilder.Models.Entities
+{
+    public class Character : EntityBase
+    {
+        public string Id { get; set; }
+
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { SetProperty(ref _name, value); }
+        }
+
+        private string _player;
+
+        public string Player
+        {
+            get { return _player; }
+            set { SetProperty(ref _player, value); }
+        }
+
+        private int _age;
+
+        public int Age
+        {
+            get { return _age; }
+            set { SetProperty(ref _age, value); }
+        }
+
+        private GenderType _gender;
+
+        public GenderType Gender
+        {
+            get { return _gender; }
+            set { SetProperty(ref _gender, value); }
+        }
+
+        private AlignmentType _alignment;
+
+        public AlignmentType Alignment
+        {
+            get { return _alignment; }
+            set { SetProperty(ref _alignment, value); }
+        }
+    }
+}
