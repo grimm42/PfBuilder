f042f3c70688a54bf09188bc2c2712c55fac1175
Nick Henkenjohann <nick.henkenjohann@gmail.com>
2018-10-13 16:26:44 +0200

    added CharacterCreatorModule and configured ModuleCatalog + set container.resolve as default viewmodel factory

---
+++ PfBuilder.CharacterCreator/CharacterCreatorModule.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Practices.Unity;
+using Prism.Modularity;
+using Prism.Regions;
+using System;
+
+namespace PfBuilder.CharacterCreator
+{
+    public class CharacterCreatorModule : IModule
+    {
+        private readonly IUnityContainer _container;
+        private readonly IRegionManager _regionManager;
+
+        public CharacterCreatorModule(IUnityContainer container, IRegionManager regionManager)
+        {
+            _container = container;
+            _regionManager = regionManager;
+        }
+
+        public void Initialize()
+        {
+                
+        }
+    }
+}
--- PfBuilder/Bootstrapper.cs
+++ PfBuilder/Bootstrapper.cs
@@ -1,11 +1,14 @@
 ﻿using Microsoft.Practices.Unity;
+using PfBuilder.CharacterCreator;
 using PfBuilder.View;
+using Prism.Modularity;
+using Prism.Mvvm;
 using Prism.Unity;
 using System.Windows;
 
 namespace PfBuilder
 {
-    class Bootstrapper : UnityBootstrapper
+    public class Bootstrapper : UnityBootstrapper
     {
         protected override DependencyObject CreateShell()
         {
@@ -15,6 +18,18 @@ namespace PfBuilder
         protected override void InitializeShell()
         {
             Application.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureModuleCatalog()
+        {
+            var catalog = (ModuleCatalog)ModuleCatalog;
+            catalog.AddModule(typeof(CharacterCreatorModule));
+        }
+
+        protected override void ConfigureContainer()
+        {
+            base.ConfigureContainer();
+            ViewModelLocationProvider.SetDefaultViewModelFactory((type) => Container.Resolve(type));
         }
     }
 }
